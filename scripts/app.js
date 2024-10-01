19c0ef4ea4902c87615ca2aa888bfa13eb155cf5
Raymond Dave Guibone <mymails.dev@gmail.com>
2024-10-01 15:26:04 +0800

    Delete js script

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-const cardSlider = document.querySelector('.pokemon-card-slider');
-const cardItems = document.querySelectorAll('.pokemon-card-item');
-
-cardItems.forEach((cardItem) => {
-  cardItem.addEventListener('click', () => {
-    cardSlider.style.animationPlayState = 'paused';
-  });
-});
